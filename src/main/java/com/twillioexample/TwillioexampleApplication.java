e365e07281477963b12b66f8b0d695952fa3d476
Prem srivastava <premsrivastavaknr@gmail.com>
2024-03-06 19:44:43 +0530

    twilio

---
+++ src/main/java/com/twillioexample/TwillioexampleApplication.java
@@ -0,0 +1,13 @@
+package com.twillioexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwillioexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwillioexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/twillioexample/TwillioexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twillioexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwillioexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
